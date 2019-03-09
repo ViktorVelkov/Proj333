46c6f8fbf13dd831073ae143ca3f1d07e6872547
ViktorVelkov <vvvvvv.velkovi@gmail.com>
2019-03-09 00:20:17 +0000

    BBBBBBB

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
+
+    public static long fib(int n) {
+        if (n <= 2)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(fib(10));
+        System.out.println("Something additional.");
     }
 }
