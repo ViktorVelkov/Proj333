2d32a394710eb668491007ecc372643ec79d7fbe
ViktorVelkov <vvvvvv.velkovi@gmail.com>
2019-03-09 01:07:23 +0000

    Message Commit

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(fib(10));
         System.out.println("Something additional.");
-        
-        System.out.println("It's a Private Repo");
+
     }
 }
